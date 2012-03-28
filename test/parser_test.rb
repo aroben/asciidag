e9189162eb17ffeba8a059c3029c30f228961a82
Adam Roben <adam@github.com>
2012-03-28 16:56:43 -0400

    Parse all test graphs in #setup
    This will make it easier to organize the tests better.

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,45 +1,20 @@
 require File.expand_path '../test_helper', __FILE__
 
 context 'Parser' do
-  test 'parses nodes' do
-    text = <<-EOF
+  setup do
+    @graph1 = Picasso.parse <<-EOF
       A---B---C topic
      /
 D---E---F---G master
 EOF
-    graph = Picasso.parse text
 
-    assert_nil find_node(graph, '/')
-
-    a = find_node graph, 'A'
-    e = find_node graph, 'E'
-    assert_equal [6, 2], a.position
-    assert_equal [e], a.parents
-
-    b = find_node graph, 'B'
-    assert_equal [10, 2], b.position
-    assert_equal [a], b.parents
-
-    d = find_node graph, 'D'
-    assert_equal [0, 0], d.position
-    assert_equal [], d.parents
-  end
-
-  test 'parses nodes with prime names' do
-    text = <<-EOF
+    @graph2 = Picasso.parse <<-EOF
                B'---C' topic
               /
 D---E---A'---F master
 EOF
-    graph = Picasso.parse text
-    assert_equal [15, 2], find_node(graph, "B'").position
-    assert_equal [20, 2], find_node(graph, "C'").position
-    assert_equal [8, 0], find_node(graph, "A'").position
-    assert_equal [13, 0], find_node(graph, 'F').position
-  end
 
-  test 'handles multiple nodes with the same label' do
-    text = <<-EOF
+    @graph3 = Picasso.parse <<-EOF
 o---o---o---o---o  master
     |            \\
     |             o'--o'--o'  topic
@@ -47,15 +22,45 @@ o---o---o---o---o  master
       o---o---o---o---o  next
 EOF
 
-    graph = Picasso.parse text
+    @graph4 = Picasso.parse <<-EOF
+      A---B---C topic
+     /         \\
+D---E---F---G---H master
+EOF
+  end
 
-    assert_nil find_node(graph, '\\')
-    assert_nil find_node(graph, '|')
+  test 'parses nodes' do
+    assert_nil find_node(@graph1, '/')
 
-    os = graph.nodes.find_all { |n| n.label == 'o' }
+    a = find_node @graph1, 'A'
+    e = find_node @graph1, 'E'
+    assert_equal [6, 2], a.position
+    assert_equal [e], a.parents
+
+    b = find_node @graph1, 'B'
+    assert_equal [10, 2], b.position
+    assert_equal [a], b.parents
+
+    d = find_node @graph1, 'D'
+    assert_equal [0, 0], d.position
+    assert_equal [], d.parents
+  end
+
+  test 'parses nodes with prime names' do
+    assert_equal [15, 2], find_node(@graph2, "B'").position
+    assert_equal [20, 2], find_node(@graph2, "C'").position
+    assert_equal [8, 0], find_node(@graph2, "A'").position
+    assert_equal [13, 0], find_node(@graph2, 'F').position
+  end
+
+  test 'handles multiple nodes with the same label' do
+    assert_nil find_node(@graph3, '\\')
+    assert_nil find_node(@graph3, '|')
+
+    os = @graph3.nodes.find_all { |n| n.label == 'o' }
     assert_equal 10, os.length
 
-    o_primes = graph.nodes.find_all { |n| n.label == "o'" }
+    o_primes = @graph3.nodes.find_all { |n| n.label == "o'" }
     assert_equal 3, o_primes.length
 
     origin = os[5]
@@ -68,17 +73,9 @@ EOF
   end
 
   test 'merge commits have multiple parents' do
-    text = <<-EOF
-      A---B---C topic
-     /         \\
-D---E---F---G---H master
-EOF
-
-    graph = Picasso.parse text
-
-    c = find_node graph, 'C'
-    g = find_node graph, 'G'
-    h = find_node graph, 'H'
+    c = find_node @graph4, 'C'
+    g = find_node @graph4, 'G'
+    h = find_node @graph4, 'H'
     assert_equal [g, c], h.parents
   end
 end
