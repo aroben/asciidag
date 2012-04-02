aroben/asciidag

commit e450c433d57b55e044ecbaeaaec1d6e98f04c539

Always treat numbers as labels, never as nodes
Fixes #7.

lib/asciidag.rb
@@ -34,7 +34,7 @@ module AsciiDag
       nodes.each_with_index do |node, i|
         node.id = "node#{i}"
       end
-      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}/ }
+      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}|\d/ }
     end
 
     def dot

test/parser_test.rb
@@ -67,6 +67,15 @@ EOF
                 |/
     A---B---C---D  master
 EOF
+
+    @graph10 = AsciiDag.parse <<-EOF
+1 2 3
+A-B-C
+     \\6 7 8
+      F-G-H
+1   2/
+D---E
+EOF
   end
 
   test 'parses nodes' do
@@ -190,5 +199,13 @@ EOF
     h = find_node @graph9, "H'"
     d = find_node @graph9, 'D'
     assert_equal [d], h.parents
+  end
+
+  test 'should treat numbers as labels' do
+    six = find_branch_label @graph10, '6'
+    assert_not_nil six
+
+    seven = find_node @graph10, '7'
+    assert_nil seven
   end
 end

commit afcfcf5cb08f9d665c0b110165cf0bc65f8a1c8b

Treat numbers that are parents as nodes, not labels
We now treat a candidate node as a label if it has no parents and is not
the parent of any other node, regardless of its label.

This fixes another regression caused by
e450c433d57b55e044ecbaeaaec1d6e98f04c539, which was fixing #7.

lib/asciidag.rb
@@ -34,7 +34,13 @@ module AsciiDag
       nodes.each_with_index do |node, i|
         node.id = "node#{i}"
       end
-      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}/ || node.label =~ /\d/ && node.parents.length == 0 }
+      all_parents = nodes.inject({}) do |parents, node|
+        node.parents.each do |parent|
+          parents[parent.id] = 1
+        end
+        parents
+      end
+      @nodes, @branch_labels = nodes.partition { |node| node.parents.length > 0 || all_parents.has_key?(node.id) }
     end
 
     def dot

test/parser_test.rb
@@ -88,6 +88,14 @@ EOF
 		/
 	---o---1---o---o---o---A
 EOF
+
+    @graph13 = AsciiDag.parse <<'EOF'
+	       o---o---o---o---C
+	      /
+	     /   o---o---o---B
+	    /   /
+	---2---1---o---o---o---A
+EOF
   end
 
   test 'parses nodes' do
@@ -213,13 +221,15 @@ EOF
     assert_equal [d], h.parents
   end
 
-  test 'should treat numbers with no parents as labels' do
+  test 'should treat disconnected numbers as labels' do
     six = find_branch_label @graph10, '6'
     assert_not_nil six
 
     seven = find_node @graph10, '7'
     assert_nil seven
+  end
 
+  test 'should treat numbers with parents as nodes' do
     one = find_node @graph12, '1'
     assert_not_nil one
     os = find_all_nodes @graph12, 'o'
@@ -227,6 +237,11 @@ EOF
 
     assert_equal [one], os[1].parents
     assert_equal [os[0]], one.parents
+  end
+
+  test 'should treat numbers that are parents as nodes' do
+    two = find_node @graph13, '2'
+    assert_not_nil two
   end
 
   test 'allows hyphens in arrowed branch labels' do