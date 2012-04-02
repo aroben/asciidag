aroben/asciidag

commit e450c433d57b55e044ecbaeaaec1d6e98f04c539

Always treat numbers as labels, never as nodes
Fixes #7.

lib/asciidag.rb
@@ -34,7 +34,7 @@ module AsciiDag
       nodes.each_with_index do |node, i|
         node.id = "node#{i}"
       end
-      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}/ }
+      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}|\d/ }
     end
 
     def dot

test/parser_test.rb
@@ -67,6 +67,15 @@ EOF
                 |/
     A---B---C---D  master
 EOF
+
+    @graph10 = AsciiDag.parse <<-EOF
+1 2 3
+A-B-C
+     \\6 7 8
+      F-G-H
+1   2/
+D---E
+EOF
   end
 
   test 'parses nodes' do
@@ -190,5 +199,13 @@ EOF
     h = find_node @graph9, "H'"
     d = find_node @graph9, 'D'
     assert_equal [d], h.parents
+  end
+
+  test 'should treat numbers as labels' do
+    six = find_branch_label @graph10, '6'
+    assert_not_nil six
+
+    seven = find_node @graph10, '7'
+    assert_nil seven
   end
 end

commit bac6201a675598c77b2ddae0345753a093aa186b

Treat numbers with parent nodes as nodes, not labels
This fixes a regression caused by
e450c433d57b55e044ecbaeaaec1d6e98f04c539, which was fixing #7.

lib/asciidag.rb
@@ -34,7 +34,7 @@ module AsciiDag
       nodes.each_with_index do |node, i|
         node.id = "node#{i}"
       end
-      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}|\d/ }
+      @branch_labels, @nodes = nodes.partition { |node| node.label =~ /[a-z]{2,}/ || node.label =~ /\d/ && node.parents.length == 0 }
     end
 
     def dot

test/parser_test.rb
@@ -82,6 +82,12 @@ EOF
   \         /
    A---B---C----------------D---E   <-- fixed-up topic branch
 EOF
+
+    @graph12 = AsciiDag.parse <<'EOF'
+		 o---o---o---B
+		/
+	---o---1---o---o---o---A
+EOF
   end
 
   test 'parses nodes' do
@@ -207,12 +213,20 @@ EOF
     assert_equal [d], h.parents
   end
 
-  test 'should treat numbers as labels' do
+  test 'should treat numbers with no parents as labels' do
     six = find_branch_label @graph10, '6'
     assert_not_nil six
 
     seven = find_node @graph10, '7'
     assert_nil seven
+
+    one = find_node @graph12, '1'
+    assert_not_nil one
+    os = find_all_nodes @graph12, 'o'
+    assert_equal 7, os.length
+
+    assert_equal [one], os[1].parents
+    assert_equal [os[0]], one.parents
   end
 
   test 'allows hyphens in arrowed branch labels' do