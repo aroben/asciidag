aroben/asciidag

commit 7bf95e8aa3b1d394ff6d11ab73d7d42337ae544f

Parse more branch labels before looking for nodes
Now we parse unarrowed branch labels, arrowed branch labels, and quoted
branch labels all before starting to look for nodes. This required
making us more strict about the type of node names we'll accept
(basically now just "x" or "x'").

As a nice side-effect, Node#label no longer returns a quoted string for
branch labels that were originally quoted. Also, quoted branch labels
that are directly adjacent to a node are no longer merged with that
node.

lib/asciidag.rb
@@ -13,7 +13,7 @@ module AsciiDag
 
     lines = text.gsub("\t", ' ' * 8).split("\n").reverse
     lines.each_with_index do |line, y|
-      branch_label = find_and_remove_arrowed_branch_label(line, y)
+      branch_label = find_and_remove_branch_label(line, y)
       branch_labels << branch_label unless branch_label.nil?
 
       i = 0
@@ -82,7 +82,7 @@ module AsciiDag
     end
 
     def dot_label
-      label.gsub("'", '&#8242;').gsub('"', '')
+      label.gsub "'", '&#8242;'
     end
 
     def inspect
@@ -93,13 +93,19 @@ module AsciiDag
 
   private
 
-  def self.find_and_remove_arrowed_branch_label(line, y)
-      match = line.match ARROWED_BRANCH_LABEL_REGEXP
+  def self.find_and_remove_branch_label(line, y)
+      match = line.match BRANCH_LABEL_REGEXP
       return if match.nil?
-      label = match[2]
-      # Position the label where the arrow starts.
-      x = match.offset(1)[0]
-      line[ARROWED_BRANCH_LABEL_REGEXP] = ''
+      group_number = match[1].nil? ? 2 : 1
+      label = match[group_number]
+      x = match.offset(group_number)[0]
+      line[BRANCH_LABEL_REGEXP] = ''
+      match = line.match ARROW_REGEXP
+      unless match.nil?
+        # Position the label where the arrow starts.
+        x = match.offset(1)[0]
+        line[ARROW_REGEXP] = ''
+      end
       Node.new label, x, y
   end
 
@@ -155,22 +161,22 @@ module AsciiDag
       return if line.nil?
       ord = line[x]
       return if ord.nil?
-      case ord.chr
-      when /[-\/\\|]/
+      if ord.chr =~ /[-\/\\|]/
         return unless valid_edge_characters.chars.include?(ord.chr)
         continue_search.call ord.chr, position, valid_direction
-      when NODE_REGEXP
+      else
         # This might be part of a multi-character node label.
-        start_x = line.rindex(/[\s\-\/\\|]/, x)
-        return if start_x.nil?
-        inner.call [start_x + 1, y], '', valid_direction
+        start_x = line.rindex(NODE_REGEXP, x)
+        return if start_x.nil? || start_x + line.substring_after(start_x)[NODE_REGEXP].length != x + 1
+        inner.call [start_x, y], 'n', valid_direction
       end
     end
     continue_search.call(:initial, position, :either).flatten
   end
 
-  NODE_REGEXP = /[^\s\-\/\\|]+/
-  ARROWED_BRANCH_LABEL_REGEXP = /\s+(<--) ([\w\s:-]+)$/
+  NODE_REGEXP = /\w'?/
+  BRANCH_LABEL_REGEXP = /(?:(\w\w.*?)|"(.+?)")\s*$/
+  ARROW_REGEXP = /\s+(<--)\s+$/
   PIXELS_PER_CHARACTER_X = 25
   PIXELS_PER_CHARACTER_Y = 40
 end

test/parser_test.rb
@@ -103,6 +103,17 @@ EOF
              \                    "topic"
               o---o---o---o---o---o
 EOF
+
+    @graph15 = AsciiDag.parse <<'EOF'
+                                "topicB"
+               o---o---o---o---o---* (pretend merge)
+              /                   /
+             /o---o---o----------'
+            |/        "topicA"
+        o---o"master"
+             \                    "topic"
+              o---o---o---o---o---o
+EOF
   end
 
   test 'parses nodes' do
@@ -257,7 +268,19 @@ EOF
   end
 
   test 'should remove quotes from branch labels' do
-    label = find_branch_label @graph14, '"master"'
+    label = find_branch_label @graph14, 'master'
+    assert_equal 'master', label.label
     assert_equal 'master', label.dot_label
+  end
+
+  test 'should not merge nodes with adjacent quoted branch labels' do
+    os = find_all_nodes @graph15, 'o'
+    o = os[7]
+    assert_equal [12, 2], o.position
+    assert_equal 'o', o.label
+    assert_equal [os[6]], o.parents
+
+    master = find_branch_label @graph15, 'master'
+    assert_not_nil master
   end
 end

commit dde5fb8b3205f2f56a2864090a56d23ba8565085

Allow node names like Y*
This was a regression caused by
7bf95e8aa3b1d394ff6d11ab73d7d42337ae544f.

lib/asciidag.rb
@@ -174,7 +174,7 @@ module AsciiDag
     continue_search.call(:initial, position, :either).flatten
   end
 
-  NODE_REGEXP = /\w'?/
+  NODE_REGEXP = /\w['*]?/
   BRANCH_LABEL_REGEXP = /(?:(\w\w.*?)|"(.+?)")\s*$/
   ARROW_REGEXP = /\s+(<--)\s+$/
   PIXELS_PER_CHARACTER_X = 25

test/parser_test.rb
@@ -114,6 +114,10 @@ EOF
              \                    "topic"
               o---o---o---o---o---o
 EOF
+
+    @graph16 = AsciiDag.parse <<'EOF'
+    ---Z---o---X--...---o---A---o---o---Y*--...---o---B*--D*
+EOF
   end
 
   test 'parses nodes' do
@@ -282,5 +286,11 @@ EOF
 
     master = find_branch_label @graph15, 'master'
     assert_not_nil master
+  end
+
+  test 'should allow asterisks in node names' do
+    b = find_node @graph16, 'B*'
+    d = find_node @graph16, 'D*'
+    assert_equal [b], d.parents
   end
 end