7b717298a93f1e66a5c9dcc5d06286f491b1d8dd
Adam Roben <adam@github.com>
2012-04-16 09:26:59 -0400

    Don't parse graphs in test setup method
    This makes it easier to debug a single test, because only the code for
    that one test will run. Most graphs were moved down into the single test
    method that uses them. Graphs that are used by more than one test were
    left in setup, but changed to just be strings instead of parsed graphs.
    The tests that use each graph now handle the parsing.

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -2,19 +2,19 @@ require File.expand_path '../test_helper', __FILE__
 
 context 'Parser' do
   setup do
-    @graph1 = AsciiDag.parse <<'EOF'
+    @graph1 = <<'EOF'
       A---B---C topic
      /
 D---E---F---G master
 EOF
 
-    @graph2 = AsciiDag.parse <<'EOF'
+    @graph2 = <<'EOF'
                B'---C' topic
               /
 D---E---A'---F master
 EOF
 
-    @graph3 = AsciiDag.parse <<'EOF'
+    @graph3 = <<'EOF'
 o---o---o---o---o  master
     |            \
     |             o'--o'--o'  topic
@@ -22,137 +22,44 @@ o---o---o---o---o  master
       o---o---o---o---o  next
 EOF
 
-    @graph4 = AsciiDag.parse <<'EOF'
+    @graph4 = <<'EOF'
       A---B---C topic
      /         \
 D---E---F---G---H master
 EOF
-
-    @graph5 = AsciiDag.parse <<'EOF'
-		E-------F
-		 \       \
-		  G---H---I---J
-			       \
-				L--M
-EOF
-
-    @graph6 = AsciiDag.parse <<'EOF'
-         o--o--o <-- Branch A
-        /
- o--o--o <-- master
-        \
-         o--o--o <-- Branch B
-EOF
-
-    @graph7 = AsciiDag.parse <<'EOF'
- o--o--O--o--o--o <-- origin
-        \        \
-         t--t--t--m <-- their branch:
-EOF
-
-    @graph8 = AsciiDag.parse <<'EOF'
-G-Y-G-W-W-W-X-X-X-X
-	   \ /
-	    W-W-B
-	   /
-Y---G-W---W
- \ /   \
-Y-Y     X-X-X-X
-EOF
-
-    @graph9 = AsciiDag.parse <<'EOF'
-                 H'--I'--J'  topicB
-                /
-                | E---F---G  topicA
-                |/
-    A---B---C---D  master
-EOF
-
-    @graph10 = AsciiDag.parse <<'EOF'
-1 2 3
-A-B-C
-     \6 7 8
-      F-G-H
-1   2/
-D---E
-EOF
-
-    @graph11 = AsciiDag.parse <<'EOF'
- P---o---o---M---x---x---W---x
-  \         /
-   A---B---C----------------D---E   <-- fixed-up topic branch
-EOF
-
-    @graph12 = AsciiDag.parse <<'EOF'
-		 o---o---o---B
-		/
-	---o---1---o---o---o---A
-EOF
-
-    @graph13 = AsciiDag.parse <<'EOF'
-	       o---o---o---o---C
-	      /
-	     /   o---o---o---B
-	    /   /
-	---2---1---o---o---o---A
-EOF
-
-    @graph14 = AsciiDag.parse <<'EOF'
-            "master"
-        o---o
-             \                    "topic"
-              o---o---o---o---o---o
-EOF
-
-    @graph15 = AsciiDag.parse <<'EOF'
-                                "topicB"
-               o---o---o---o---o---* (pretend merge)
-              /                   /
-             /o---o---o----------'
-            |/        "topicA"
-        o---o"master"
-             \                    "topic"
-              o---o---o---o---o---o
-EOF
-
-    @graph16 = AsciiDag.parse <<'EOF'
-    ---Z---o---X--...---o---A---o---o---Y*--...---o---B*--D*
-EOF
-
-    @graph17 = AsciiDag.parse <<'EOF'
-    r1---r2---r3 remotes/git-svn
-                \
-                 A---B master
-EOF
   end
 
   test 'parses nodes' do
-    a = find_node @graph1, 'A'
-    e = find_node @graph1, 'E'
+    graph = AsciiDag.parse @graph1
+
+    a = find_node graph, 'A'
+    e = find_node graph, 'E'
     assert_equal [6, 2], a.position
     assert_equal [e], a.parents
 
-    b = find_node @graph1, 'B'
+    b = find_node graph, 'B'
     assert_equal [10, 2], b.position
     assert_equal [a], b.parents
 
-    d = find_node @graph1, 'D'
+    d = find_node graph, 'D'
     assert_equal [0, 0], d.position
     assert_equal [], d.parents
   end
 
   test 'parses nodes with prime names' do
-    assert_equal [15, 2], find_node(@graph2, "B'").position
-    assert_equal [20, 2], find_node(@graph2, "C'").position
-    assert_equal [8, 0], find_node(@graph2, "A'").position
-    assert_equal [13, 0], find_node(@graph2, 'F').position
+    graph = AsciiDag.parse @graph2
+
+    assert_equal [15, 2], find_node(graph, "B'").position
+    assert_equal [20, 2], find_node(graph, "C'").position
+    assert_equal [8, 0], find_node(graph, "A'").position
+    assert_equal [13, 0], find_node(graph, 'F').position
   end
 
   test 'does not count edges as nodes' do
     ['-', '/', '|', '\\'].each do |char|
       (1..4).each do |i|
         graph_name = "@graph#{i}"
-        graph = self.instance_variable_get(graph_name)
+        graph = AsciiDag.parse self.instance_variable_get(graph_name)
         node = find_node(graph, char)
         assert_nil node, "Found unexpected node #{node.inspect} in #{graph_name}"
       end
@@ -160,10 +67,12 @@ EOF
   end
 
   test 'handles multiple nodes with the same label' do
-    os = find_all_nodes @graph3, 'o'
+    graph = AsciiDag.parse @graph3
+
+    os = find_all_nodes graph, 'o'
     assert_equal 10, os.length
 
-    o_primes = find_all_nodes @graph3, "o'"
+    o_primes = find_all_nodes graph, "o'"
     assert_equal 3, o_primes.length
 
     origin = os[5]
@@ -177,62 +86,101 @@ EOF
   end
 
   test 'merge commits have multiple parents' do
-    c = find_node @graph4, 'C'
-    g = find_node @graph4, 'G'
-    h = find_node @graph4, 'H'
+    graph = AsciiDag.parse @graph4
+
+    c = find_node graph, 'C'
+    g = find_node graph, 'G'
+    h = find_node graph, 'H'
     assert_equal [c, g], h.parents
   end
 
   test 'nodes get unique IDs' do
     ids = {}
-    @graph3.nodes.each do |node|
+    AsciiDag.parse(@graph3).nodes.each do |node|
       assert !ids.has_key?(node.id), "Found duplicate IDs: #{node.inspect}, #{ids[node.id].inspect}"
       ids[node.id] = node
     end
   end
 
   test 'branch labels are separate from nodes' do
-    master = @graph1.branch_labels.find { |l| l.label == 'master' }
+    graph = AsciiDag.parse @graph1
+
+    master = graph.branch_labels.find { |l| l.label == 'master' }
     assert master
-    assert_nil find_node(@graph1, 'master')
+    assert_nil find_node(graph, 'master')
   end
 
   test 'treats tabs as 8 spaces' do
-    l = find_node @graph5, 'L'
-    j = find_node @graph5, 'J'
+    graph = AsciiDag.parse <<'EOF'
+		E-------F
+		 \       \
+		  G---H---I---J
+			       \
+				L--M
+EOF
+
+    l = find_node graph, 'L'
+    j = find_node graph, 'J'
     assert_equal [j], l.parents
   end
 
   test 'recognizes branch label arrows' do
-    assert_nil find_node(@graph6, '<')
+    graph = AsciiDag.parse <<'EOF'
+         o--o--o <-- Branch A
+        /
+ o--o--o <-- master
+        \
+         o--o--o <-- Branch B
+EOF
 
-    branch_a = find_branch_label @graph6, 'Branch A'
+    assert_nil find_node(graph, '<')
+
+    branch_a = find_branch_label graph, 'Branch A'
     assert_not_nil branch_a
     assert_equal [17, 4], branch_a.position
 
-    master = find_branch_label @graph6, 'master'
+    master = find_branch_label graph, 'master'
     assert_not_nil master
     assert_equal [9, 2], master.position
 
-    branch_b = find_branch_label @graph6, 'Branch B'
+    branch_b = find_branch_label graph, 'Branch B'
     assert_not_nil branch_b
     assert_equal [17, 0], branch_b.position
+  end
 
-    assert_nil find_node(@graph7, '<')
+  test 'should allow colons in branch labels' do
+    graph = AsciiDag.parse <<'EOF'
+ o--o--O--o--o--o <-- origin
+        \        \
+         t--t--t--m <-- their branch:
+EOF
 
-    their_branch = find_branch_label @graph7, 'their branch:'
+    assert_nil find_node(graph, '<')
+
+    their_branch = find_branch_label graph, 'their branch:'
     assert_not_nil their_branch
     assert_equal [20, 0], their_branch.position
   end
 
   test 'should replace apostrophes with primes' do
-    a = find_node @graph2, "A'"
+    graph = AsciiDag.parse @graph2
+    a = find_node graph, "A'"
     assert_equal "A&#8242;", a.dot_label
   end
 
   test 'should only follow edges in allowed directions' do
-    xs = find_all_nodes @graph8, 'X'
-    ws = find_all_nodes @graph8, 'W'
+    graph = AsciiDag.parse <<'EOF'
+G-Y-G-W-W-W-X-X-X-X
+	   \ /
+	    W-W-B
+	   /
+Y---G-W---W
+ \ /   \
+Y-Y     X-X-X-X
+EOF
+
+    xs = find_all_nodes graph, 'X'
+    ws = find_all_nodes graph, 'W'
 
     x = xs[4]
     w = ws[6]
@@ -244,23 +192,46 @@ EOF
   end
 
   test 'should follow pipe underneath slash' do
-    h = find_node @graph9, "H'"
-    d = find_node @graph9, 'D'
+    graph = AsciiDag.parse <<'EOF'
+                 H'--I'--J'  topicB
+                /
+                | E---F---G  topicA
+                |/
+    A---B---C---D  master
+EOF
+
+    h = find_node graph, "H'"
+    d = find_node graph, 'D'
     assert_equal [d], h.parents
   end
 
   test 'should treat disconnected numbers as labels' do
-    six = find_branch_label @graph10, '6'
+    graph = AsciiDag.parse <<'EOF'
+1 2 3
+A-B-C
+     \6 7 8
+      F-G-H
+1   2/
+D---E
+EOF
+
+    six = find_branch_label graph, '6'
     assert_not_nil six
 
-    seven = find_node @graph10, '7'
+    seven = find_node graph, '7'
     assert_nil seven
   end
 
   test 'should treat numbers with parents as nodes' do
-    one = find_node @graph12, '1'
+    graph = AsciiDag.parse <<'EOF'
+		 o---o---o---B
+		/
+	---o---1---o---o---o---A
+EOF
+
+    one = find_node graph, '1'
     assert_not_nil one
-    os = find_all_nodes @graph12, 'o'
+    os = find_all_nodes graph, 'o'
     assert_equal 7, os.length
 
     assert_equal [one], os[1].parents
@@ -268,41 +239,82 @@ EOF
   end
 
   test 'should treat numbers that are parents as nodes' do
-    two = find_node @graph13, '2'
+    graph = AsciiDag.parse <<'EOF'
+	       o---o---o---o---C
+	      /
+	     /   o---o---o---B
+	    /   /
+	---2---1---o---o---o---A
+EOF
+
+    two = find_node graph, '2'
     assert_not_nil two
   end
 
   test 'allows hyphens in arrowed branch labels' do
-    label = find_branch_label @graph11, 'fixed-up topic branch'
+    graph = AsciiDag.parse <<'EOF'
+ P---o---o---M---x---x---W---x
+  \         /
+   A---B---C----------------D---E   <-- fixed-up topic branch
+EOF
+
+    label = find_branch_label graph, 'fixed-up topic branch'
     assert_not_nil label
   end
 
   test 'should remove quotes from branch labels' do
-    label = find_branch_label @graph14, 'master'
+    graph = AsciiDag.parse <<'EOF'
+            "master"
+        o---o
+             \                    "topic"
+              o---o---o---o---o---o
+EOF
+
+    label = find_branch_label graph, 'master'
     assert_equal 'master', label.label
     assert_equal 'master', label.dot_label
   end
 
   test 'should not merge nodes with adjacent quoted branch labels' do
-    os = find_all_nodes @graph15, 'o'
+    graph = AsciiDag.parse <<'EOF'
+                                "topicB"
+               o---o---o---o---o---* (pretend merge)
+              /                   /
+             /o---o---o----------'
+            |/        "topicA"
+        o---o"master"
+             \                    "topic"
+              o---o---o---o---o---o
+EOF
+
+    os = find_all_nodes graph, 'o'
     o = os[7]
     assert_equal [12, 2], o.position
     assert_equal 'o', o.label
     assert_equal [os[6]], o.parents
 
-    master = find_branch_label @graph15, 'master'
+    master = find_branch_label graph, 'master'
     assert_not_nil master
   end
 
   test 'should allow asterisks in node names' do
-    b = find_node @graph16, 'B*'
-    d = find_node @graph16, 'D*'
+    graph = AsciiDag.parse <<'EOF'
+    ---Z---o---X--...---o---A---o---o---Y*--...---o---B*--D*
+EOF
+
+    b = find_node graph, 'B*'
+    d = find_node graph, 'D*'
     assert_equal [b], d.parents
   end
 
   test 'should number-suffixed node names' do
-    r1 = find_node @graph17, 'r1'
-    r2 = find_node @graph17, 'r2'
+    graph = AsciiDag.parse <<'EOF'
+    r1---r2---r3 remotes/git-svn
+                \
+                 A---B master
+EOF
+    r1 = find_node graph, 'r1'
+    r2 = find_node graph, 'r2'
     assert_equal [], r1.parents
     assert_equal [r1], r2.parents
   end
